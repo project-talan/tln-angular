8ded1f1f0bccc91f9c29f270806fcdd7f8d5f9b3
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-08-20 13:28:22 +0000

    env.sh

--- .env.sh
+++ .env.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -e
 
-# can be used for standalon project
-#: '
-export $(cat ./.env | grep -v ^# | xargs)
-#'
+if [ -f './../../.env' ]; then
+  echo '## Export variables from project .env file'
+  export $(cat ./../../.env | grep -v ^# | xargs)
+  export COMPONENT_ID="${ID}"
+  export COMPONENT_VERSION=${PROJECT_VERSION}
+  export COMPONENT_PARAM_HOST=${HOST}
+  export COMPONENT_PARAM_LSTN=${LSTN}
+  export COMPONENT_PARAM_PORT=${PORT}
+  export COMPONENT_PARAM_PORTS=${PORTS}
 
-# export configuration from the top level of microservice
-: '
-export $(cat ./../../.env | grep -v ^# | xargs)
-export COMPONENT_VERSION=${PROJECT_VERSION}
-export COMPONENT_ID="${ID}"
-export COMPONENT_PARAM_HOST=${HOST}
-export COMPONENT_PARAM_LSTN=${LSTN}
-export COMPONENT_PARAM_PORT=${PORT}
-export COMPONENT_PARAM_PORTS=${PORTS}
-'
+  # project specific mappings
+fi
 
-printenv | grep COMPONENT_
+if [ -f './.env' ]; then
+  echo '## Export variables from local .env file'
+  export $(cat ./.env | grep -v ^# | xargs)
+fi
+
+printenv | grep COMPONENT_ | sort
